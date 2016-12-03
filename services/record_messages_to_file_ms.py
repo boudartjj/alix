437c62919a39ed3fae7f2ae3ec338d346b29c273
root <root@ip-172-31-35-161.ec2.internal>
2016-12-03 19:30:08 +0000

    release 0.4

--- services/record_messages_to_file_ms.py
+++ services/record_messages_to_file_ms.py
@@ -1,16 +1,6 @@
-#!/usr/bin/env python
-
-import sys
-from alix import Alix
-
 class MicroService(Alix):
 	def onMessage(self, message):
 		#save error message into file
 		fo = open(self.getParam('path'), 'a')
 		fo.write(message + '\n')
 		fo.close() 
-
-#get micro service name
-name = sys.argv[1]
-
-MicroService(name).start()
--- services/skeleton_ms.py
+++ services/skeleton_ms.py
@@ -1,16 +1,6 @@
-#!/usr/bin/env python
-
-#this class is micro service skeleton
-
-import sys
 from alix import Alix
 
 class MicroService(Alix):
 	def onMessage(self, message):
 		#implement your code here
 		pass
-
-#get micro service name
-name = sys.argv[1]
-
-MicroService(name).start()
